504813a115f5a230c1d879ea7e52a236c86375e7
mperezr1 <manuelestebanpr@gmail.com>
2018-11-15 14:27:57 -0500

    no more pls

--- src/app/Components/preguntas/modificar/modificar-Eliminar/modificar-Eliminar.component.ts
+++ src/app/Components/preguntas/modificar/modificar-Eliminar/modificar-Eliminar.component.ts
@@ -24,7 +24,7 @@ export class ModificarEliminarComponent implements OnInit {
     });
   }
 
-  putPregunta(form: NgForm){
+  putPreguntas(form: NgForm){
     if (form.invalid) {
       return;
     }
